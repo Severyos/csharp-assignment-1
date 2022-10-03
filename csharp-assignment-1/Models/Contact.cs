481ec734a17138913a8c06dbaa79de7c8e15ef2d
Simon Simonsson <simon_bklina@hotmail.com>
2022-10-03 19:44:36 +0200

    Property added

--- csharp-assignment-1/Models/Contact.cs
+++ csharp-assignment-1/Models/Contact.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 namespace csharp_assignment_1.Models
 {
-    internal class Contact
+    public class Contact
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string StreetName { get; set; }
+        public string Adress { get; set; }
         public string PostalCode { get; set; }
         public string City { get; set; }
+
+        public Contact(string firstName, string lastName, string adress, string postalCode, string city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Adress = adress;
+            PostalCode = postalCode;
+            City = city;
+        }
     }
 }
